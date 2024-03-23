b473973375c596c9db0631cb717ac77cc19301fc
410am <rlclqk@naver.com>
2024-03-23 17:49:34 +0900

    로그인 ui

--- frontend/src/pages/EntrancePage/index.tsx
+++ frontend/src/pages/EntrancePage/index.tsx
@@ -1,3 +1,5 @@
+import SignIn from "./SignIn";
+
 const EntrancePage = () => {
   // 1. 로그인
   // 2. 회의 참가
@@ -5,12 +7,12 @@ const EntrancePage = () => {
   // 2,3번 묶으면 될듯
 
   return (
-    <>
-      <div>머여</div>
-      <div className="font-['Nanum Gothic']">된건가</div>
-
-      <div className="material-icons text-xs">upload</div>
-    </>
+    <div className="">
+      {/* <div>머여</div> */}
+      {/* <div className="font-['Nanum Gothic']">된건가</div> */}
+      {/* <div className="material-icons text-xs">upload</div> */}
+      <SignIn />
+    </div>
   );
 };
 
